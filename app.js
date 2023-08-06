6e83a51561dc3ac5c0f4cd4a726f1af401f277d4
sandradenis <130932173+sandradenis@users.noreply.github.com>
2023-08-06 19:00:25 -0300

    Second commit

--- app.js
+++ app.js
@@ -95,3 +95,16 @@ function siguiente(){
 }
 
 noticias.fetchNoticias(temaActual);
+
+//comentarios
+const submitButton = document.getElementById("submit-button");
+const commentInput = document.getElementById("comment-input");
+const commentsContainer = document.getElementById("comments-container");
+
+submitButton.addEventListener("click", function() {
+  const comment = commentInput.value;
+  const commentElement = document.createElement("div");
+  commentElement.innerHTML = comment;
+  commentsContainer.appendChild(commentElement);
+  commentInput.value = "";
+});

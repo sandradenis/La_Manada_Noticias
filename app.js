c9a00445d70661caa61bbdb49dd167185b5e54ae
sandradenis <130932173+sandradenis@users.noreply.github.com>
2023-08-09 20:36:14 -0300

    3er commit

--- app.js
+++ app.js
@@ -1,110 +1,29 @@
-//Cantida de noticias que se cargaran cada vez que se presione siguiente (5 + 1)
-let cantidadNoticias = 5;
-let pageFinal = cantidadNoticias;
-let pageInicial = 0;
-let temaActual = "Tecnología";
+let cad1 = `
+<h2 class="titulo">Noticias actualizadas</h2>
+<br><br>
 
-let noticias = {
-    "apiKey":"6fcae07a1716403db5758d52b56d00fb",
-    fetchNoticias:function(categoria){
-        fetch(
-            "https://newsapi.org/v2/everything?q="
-            +categoria+
-            "&languaje=es&apiKey="+this.apiKey
-        )
-        .then((response)=>response.json())
-        .then((data)=>this.displayNoticias(data));
-    },
-    displayNoticias: function(data){
-        //elimino todo si ha seleccionado un nuevo tema
-        if(pageInicial==0){
-            document.querySelector(".container-noticias").textContent ="";
-        }
+<div class="containerNovedades">
+`;
 
-
-        for(i=pageInicial;i<=pageFinal;i++){
-            const {title} = data.articles[i];
-            let h2 = document.createElement("h2");
-            h2.textContent = title;
-    
-            const {urlToImage} = data.articles[i];
-            let img = document.createElement("img");
-            img.setAttribute("src", urlToImage);
-
-            let info_item = document.createElement("div");
-            info_item.className = "info_item";
-            const {publishedAt} = data.articles[i];
-            let fecha = document.createElement("span");
-            let date = publishedAt;
-            date=date.split("T")[0].split("-").reverse().join("-");
-            fecha.className = "fecha";
-            fecha.textContent = date;
-
-            const {name} = data.articles[i].source;
-            let fuente = document.createElement("span");
-            fuente.className = "fuente";
-            fuente.textContent = name;
-
-            info_item.appendChild(fecha);
-            info_item.appendChild(fuente);
-
-            const {url} = data.articles[i];
-
-            let item = document.createElement("div");
-            item.className = "item";
-            item.appendChild(h2);
-            item.appendChild(img);
-            item.appendChild(info_item);
-            item.setAttribute("onclick", "location.href='"+url+"'");
-            document.querySelector(".container-noticias").appendChild(item);
-        }
-
-        let btnSiguiente = document.createElement("span");
-        btnSiguiente.id = "btnSiguiente";
-        btnSiguiente.textContent = "Ver más";
-        btnSiguiente.setAttribute("onclick","siguiente()");
-        document.querySelector(".container-noticias").appendChild(btnSiguiente);
-    }
+for (let elemento of data) {
+    cad1 += `
+        <div class="card">
+            <img src="${elemento.image_url}">
+            <div class="card-img">
+                <h2>${elemento.title}</h2>
+                <h3>${elemento.pubDate}</h3>
+                <ul>
+                    <li>${elemento.source_id}</li>
+                    <li>${elemento.creator}</li>
+                    <li>${elemento.country}</li>
+                </ul>
+            </div>
+        </div>
+    `;
 }
 
+cad1 += `
+</div>
+`;
 
-
-function buscar(cat){
-    pageInicial = 0;
-    pageFinal = cantidadNoticias;
-    temaActual = cat;
-    noticias.fetchNoticias(cat);
-}
-
-function buscarTema(){
-    pageInicial = 0;
-    pageFinal = cantidadNoticias;
-
-    let tema = document.querySelector("#busqueda").value;
-    temaActual = tema;
-    noticias.fetchNoticias(temaActual);
-}
-
-function siguiente(){
-    pageInicial = pageFinal + 1;
-    pageFinal = pageFinal + cantidadNoticias + 1;
-    //eliminamos el botón siguiente
-    document.querySelector("#btnSiguiente").remove();
-    noticias.fetchNoticias(temaActual);
-
-}
-
-noticias.fetchNoticias(temaActual);
-
-//comentarios
-const submitButton = document.getElementById("submit-button");
-const commentInput = document.getElementById("comment-input");
-const commentsContainer = document.getElementById("comments-container");
-
-submitButton.addEventListener("click", function() {
-  const comment = commentInput.value;
-  const commentElement = document.createElement("div");
-  commentElement.innerHTML = comment;
-  commentsContainer.appendChild(commentElement);
-  commentInput.value = "";
-});
+// Now you can use the 'cad1' HTML string to update the content on your webpage
---
+++ noticias.js
@@ -0,0 +1,142 @@
+
+const data = [
+    {
+        "title": "“¡Vos sos enfermera, vení!”: el dramático relato de las mamás que asistieron a Morena",
+        "link": "",
+        "keywords": ["Derechos Humanos"],
+        "creator": "Walter Vazquez",
+        "video_url": null, 
+        "content": "Daiana y Karina fueron dos de las primeras personas que socorrieron a la nena de 11 años tras el salvaje ataque motochorro",
+        "pubDate": "2023-08-09 18:58:12",
+        "image_url": "https://www.infobae.com/new-resizer/X5Sew_9_6GaH1U83A-2Po1ntml8=/768x512/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/DXKN3KSNPFCB3HK7XXOGQRE34Q.jpg",
+        "source_id": "infobae",
+        "category": ["top"],
+        "country": ["Argentina"],
+        "language": "spanish"
+    },
+    {
+        "title": "“Dale Boca”: la publicación de Edinson Cavani horas antes de su debut que encendió la ilusión de los hinchas", 
+        "link": "https://www.infobae.com/deportes/2023/08/09/dale-boca-la-publicacion-de-edinson-cavani-horas-antes-de-su-debut-que-encendio-la-ilusion-de-los-hinchas/",
+        "keywords": ["Jorge Jouroff"],
+        "creator": null,
+        "video_url": null, "description": null,
+        "content": "Así palpita el uruguayo la revancha de los octavos de final de la Copa Libertadores ante Nacional en La Bombonera",
+        "pubDate": "2023-08-09 18:58:12",
+        "image_url": "https://www.infobae.com/new-resizer/Lf-WiB7JDsJ2vIIJf11cb-HGU1Y=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/X2HNIM6O3FCN5HA23SPSLUNASE.jpg",
+        "source_id": "infobae",
+        "category": ["top"],
+        "country": ["Uruguay"],
+        "language": "spanish"
+    },
+    {
+        "title": "Cómo será el festejo del cumpleaños de 15 de Indiana, la hija mayor de Nicole Neumann y Fabián Cubero",
+        "link": "https://www.infobae.com/teleshow/2023/08/09/como-sera-el-festejo-del-cumpleanos-de-15-de-indiana-la-hija-mayor-de-nicole-neumann-y-fabian-cubero/",
+        "keywords": ["Columnistas"],
+        "creator": null, 
+        "video_url": "https://cdn.jwplayer.com/previews/papaa5G9", 
+        "content": "La adolescente hizo un pedido especial a su padre para celebrar esta nueva etapa de su vida",
+        "pubDate": "2023-08-09 18:58:12",
+        "image_url": "https://www.infobae.com/new-resizer/4kR17hVppUFbsCEtSYhS4aUwTUk=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/OSQEZDOEZVFCJD6NUXLPLDHNKA.jpg",
+        "source_id": "uypress",
+        "category": ["top"],
+        "country": ["Uruguay"],
+        "language": "spanish"
+    },
+    {
+        "title": "La subvariante EG.5 ya está en Argentina, ¿podrían aumentar los casos de COVID?",
+        "link": "https://www.infobae.com/salud/ciencia/2023/08/09/la-subvariante-eg5-ya-esta-en-argentina-podrian-aumentar-los-casos-de-covid/",
+        "keywords": ["Ciencia"],
+        "creator": "Valeria Román", 
+        "video_url": null,
+        "content": "La presencia en el territorio nacional del sublinaje de Ómicron, conocido popularmente como Eris, fue informada a Infobae por la doctora Mariana Viegas, coordinadora del Proyecto País. Cuáles son los síntomas y sus riesgos, según expertos en virología",
+        "image_url": "https://www.infobae.com/new-resizer/NNSc8GhsFk7e5CWxtzhWZXq8sAg=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/I67V7RE62REQNAUUCDPZEJLPWY.jpg",
+        "source_id": "Infobae",
+        "category": ["top"],
+        "country": ["Argentina"],
+        "language": "spanish"
+    },
+    {
+        "title": "La provocación de un grupo mapuche que será juzgado por usurpar tierras en Bariloche: “Lo tomamos con soda”",
+        "link": "https://www.infobae.com/politica/2023/08/09/la-provocacion-de-un-grupo-mapuche-que-sera-juzgado-por-usurpar-tierras-en-bariloche-lo-tomamos-con-soda/",
+        "keywords": ["Polita", "Sociedad", "Todas"],
+        "creator": ["Francisco Poppe"], 
+        "video_url": null, 
+        "content": "Son integrantes de la Comunidad Lof Che Buenuleo y el lonko de la comunidad Pillan Mahuiza. La palabra de uno de los imputados",
+        "pubDate": "2023-08-09 18:55:55",
+        "image_url": "https://www.infobae.com/new-resizer/ky1hKKaIn7GQ3bLWTaI2ttARGXk=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/LUAYGA2BXFCOVMSLWM2BYWP5VE.jpg", 
+        "source_id": "24horas",
+        "category": ["top"],
+        "country": ["Argentina"],
+        "language": "spanish"
+    },
+    {
+        "title": "Dónde voto: consultá el padrón electoral definitivo para las PASO y elecciones nacionales 2023",
+        "link": "https://www.infobae.com/politica/2023/07/14/donde-voto-consulta-el-padron-definitivo-para-las-elecciones-2023/",
+        "keywords": ["Politica", "Todas", "0003estados", "construcción", "Mario Delgado", "Morena", "rescate", "Tren Maya", "zonas arqueológicas"],
+        "creator": null,
+        "video_url": null,
+        "content": "Cómo chequear los datos necesarios para votar en las primarias del 13 de agosto y las generales del 22 de octubre",
+        "pubDate": "2023-08-09 18:55:21",
+        "image_url": "https://www.infobae.com/new-resizer/NnlYkydKjCwK-AUHzSt0d14QcXk=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/OKHSRQHOKBGSJPOBOOJXHPFE7A.jpg", 
+        "source_id": "24horas",
+        "category": ["top"],
+        "country": ["Argentina"],
+        "language": "spanish"
+    },
+    {
+        "title": "Boca Juniors recibe a Nacional con el objetivo de avanzar a los cuartos de final de la Copa Libertadores: hora, TV y formaciones",
+        "link": "https://www.infobae.com/deportes/2023/08/09/boca-juniors-recibe-a-nacional-con-el-objetivo-de-avanzar-a-los-cuartos-de-final-de-la-copa-libertadores-hora-tv-y-formaciones/",
+        "keywords": ["Redacción 24 Horas"],
+        "creator": null,
+        "video_url": null,
+        "content": "El equipo de Jorge Almirón se enfrenta al conjunto uruguayo por la revancha de la serie que está 0-0. Desde las 21, transmiten Fox Sports, Telefe y Star+",
+        "pubDate": "2023-08-09 18:52:15",
+        "image_url": "https://www.infobae.com/new-resizer/cwlTLiNyyrv-S08XrcougEZg22U=/768x1024/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/2R24BI3NAZAYJPOQHEEVTBBL3I.jpg",
+        "source_id": "24horas",
+        "category": ["top"],
+        "country": ["Argentina"],
+        "language": "spanish"
+    },
+    {
+        "title": "La Justicia rehen de la Vicepresidenta",
+        "link": "https://www.infobae.com/opinion/2023/08/09/la-justicia-rehen-de-la-vicepresidenta/",
+        "keywords": ["Redacción 24 Horas"],
+        "creator": "Silvina Martínez",
+        "video_url": null,
+        "content": "Alumna de 11 años muere tras ser asaltada y golpeada cuando llegaba a la escuela en Lanús VER VIDEO Una niña de 11 años murió este miércoles luego de ser víctima de un asalto cometido por motochorros que la atacaron y arrastraron para robarle sus pertenencias cuando llegaba caminando al colegio en el partido bonaerense de Lanús, y por el caso fueron detenidos dos jóvenes mayores de edad como sospechosos, informaron fuentes policiales, judiciales y del municipio. El ministro de Seguridad bonaerense, Sergio Berni , aseguró que se trata de \"un hecho lamentable, triste y conmovedor\", confirmó que \"ya están detenidos\" los dos jóvenes que presuntamente participaron en el crimen y que fue secuestrada la moto y los dos cascos que se cree que utilizaron en el hecho, por lo que \"el caso está cerrado\" . En tanto, esta tarde, decenas de vecinos del barrio donde ocurrió el hecho protagonizaban algunos incidentes y arrojaban piedras contra el frente a la comisaría 5ta. de Lanús, ubicada en la calle Warnes al 3000 de Villa Diamante. Foto: Eliana Obregón. En esa seccional policial se hallaban alojados los dos jóvenes detenidos como así también al menos tres adolescentes, a quienes los policías demoraron ya que en la casa de alguno de ellos se secuestró la moto que presuntamente se utilizó en el hecho. En tanto, allegados a la víctima convocaron a una movilización este miércoles para reclamar justicia por la muerte de la niña, identificada como Morena Domínguez , y más seguridad en la zona. Foto: Pepe Mateos El hecho ocurrió minutos antes de las 7.30 de esta mañana, cuando la niña fue asaltada en momentos en que llegaba a la Escuela Almafuerte N° 60, situada en la calle Molinedo al 3200, de la localidad de Villa Diamante, partido de Lanús. La secuencia quedó grabada por una cámara de seguridad que se convirtió en una prueba clave de una causa que, según las fuentes, terminará caratulada como homicidio. El padre de Morena. Foto: Pepe Mateos En el video se observa que Morena cae al asfalto cuando es abordada por los dos motochorros, tras lo cual el que viajaba como acompañante se bajó para agarrar alguna de sus pertenencias y luego volvió a abordar el vehículo para escapar. Un automovilista que fue testigo de la secuencia intentó encerrar a los delincuentes, que también fueron corridos por un barrendero, pero éstos lograron escapar del lugar a toda velocidad, según quedó también registrado por la cámara. Foto: Pepe Mateos \"Veo que una nena está colgada de una moto, luchando con la mochila. Tiro el auto encima, no para pegarle porque estaba la nena. Se suben arriba de la vereda y me apuntan con un revólver. No llegué a ver si tenían patente. Después volví y había otro señor que la ayudaba. No la vi desvanecida, la vi mareada. No vi si se cayó al piso. Cuando se cae, yo ya había pasado . Todos los días están robando acá\", aseguró esta mañana uno de los hombres que quiso ayudar a Morena, quien se identificó como Damián. Foto: Eliana Obregón. También se vio que, luego de la huida de los delincuentes, un hombre se acercó a asistir a la víctima, pero la niña quedó sentada en el asfalto sin poder reincorporarse. Fuentes policiales indicaron que, en la caída, la chica se golpeó en la cabeza y que ello pudo tener relación con el cuadro que presentaba cuando llegó la ambulancia del SAME. Foto: Eliana Obregón. Morena se descompensó mientras esperaba la asistencia y, cuando llegó la ambulancia, el médico realizó maniobras de reanimación y la trasladaron en estado crítico al Hospital Evita, donde falleció producto de un paro cardiorrespiratorio, según informó la policía. El caso es investigado en principio por la fiscal Silvia Bussano , de la Unidad Funcional de Instrucción (UFI) 7 de Lanús, quien concurrió esta mañana al lugar del hecho para impartir las directivas de la investigación. La fiscal dispuso además que el cuerpo de la víctima sea trasladado a la morgue judicial de Lomas de Zamora, donde se intentará determinar si la causa del deceso está vinculada a algún traumatismo de cráneo o a otro motivo. Diego Kravetz , secretario de Seguridad e intendente interino de Lanús, dijo mediante un comunicado difundido en sus redes sociales que el municipio y la policía están realizando un seguimiento de las cámaras de seguridad del barrio y alrededores para dar con los sospechosos, los que finalmente fueron apresados por personal de la policía bonaerense, uno de ellos identificado por la policía como Miguel Ángel Madariaga (28). Foto: Eliana Obregón. \"Cuando los malvivientes le sustraen el teléfono celular le ocasionan un golpe en la cabeza que la deja inconsciente, la ambulancia del SAME la intenta reanimar en el lugar y la traslada en estado crítico al Hospital Evita, donde fallece 20 minutos después de haber ingresado\", tuiteó el funcionario, quien agregó que en ese mismo barrio, hace un mes, él mismo detuvo a un \"motochorro\" que intentó robarle a un vecino. En tanto, el gobernador de la provincia de Buenos Aires y precandidato a la reelección, Axel Kicillof, suspendió el acto de cierre de campaña electoral en el municipio de Merlo como consecuencia de este hecho, al igual que Néstor Grindetti, intendente en licencia de Lanús y precandidato a gobernador bonaerense por Juntos por el Cambio. Foto: Eliana Obregón. Florencia , madre de una compañera de Morena, relató al canal Todo Noticias que, luego del ataque, intentaron reanimar a la niña pero que estaba inconsciente. \"Yo estaba en la puerta del colegio, se escuchan los gritos, me acerco, la nena ya estaba tirada en el piso, estaba consciente como lo ven en el video. Le arrebatan el celular y el barrendero es quien la rescata y pega el grito de ayuda. Sé que le pegan dos piñas en el estómago a la nena, le arrebatan el celular y la arrastran. La levantamos y la llevamos al colegio. Después, la nena ya estaba inconsciente y no respondía\", concluyó la mujer, acongojada. Foto: Pepe Mateos \" Esas cuadras donde está el colegio es zona de nadie . Muchas veces reclamaron seguridad. ¿Tiene que perder la vida un chico para que haya seguridad?\", se preguntó María Domínguez , la madre de Morena, quien se encuentra en la provincia de Salta, donde viajó para bautizar a otro de sus hijos. En diálogo con el canal A24, la mujer hizo así referencia a la zona dónde se encuentra emplazada la Escuela Almafuerte N° 60, situada en la calle Molinedo al 3200, de la localidad de Villa Diamante, donde asistía la niña.",
+        "pubDate": "2023-08-09 18:50:00",
+        "image_url": "https://www.infobae.com/new-resizer/hvUkiJ4eRqj6gYYnY_fPvzb3pEo=/768x432/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/FXQCRT4FI5ADLPVCQDUYADHSPQ.jpg",
+        "source_id": "telam",
+        "category": ["top"],
+        "country": ["argentina"],
+        "language": "spanish"
+    },
+    ]
+
+    let cad1 = `
+    <h2 class="titulo">Noticias actualizadas</h2>
+    <br><br>
+    
+    <div class="containerNovedades">
+    `;
+    
+    for (let elemento of data) {
+        cad1 += `
+            <div class="card">
+                <img src="${elemento.image_url}">
+                <div class="card-img">
+                    <h2>${elemento.title}</h2>
+                    <h3>${elemento.pubDate}</h3>
+                    <ul>
+                        <li>${elemento.source_id}</li>
+                        <li>${elemento.creator}</li>
+                        <li>${elemento.country}</li>
+                    </ul>
+                </div>
+            </div>
+        `;
+    }
+    
+    cad1 += `
+    </div>
+    `;
